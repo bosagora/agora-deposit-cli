fa6aa4f810265745d8beb441c2edb6bf64b9691e
Ömer Faruk Irmak <omerfirmak@gmail.com>
2022-09-02 10:53:54 +0300

    add option to skip verification on newly generated files

--- staking_deposit/utils/config.py
+++ staking_deposit/utils/config.py
@@ -5,3 +5,4 @@ This file contains global variables to required to parameterise click functional
 language = 'en'  # The CLI language selected by the user
 non_interactive = False  # Whether or not to interactively prompt the user for input. (Useful for tests and debugging)
 real_non_interactive = False  # Really be non-interactive (as tests rely on non_interactive but with output still)
+skip_verify = False # skip verification on generated keys (for testing purposes)
